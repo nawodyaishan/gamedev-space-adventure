bf3990de0a122642a5543cdcc40974819d7f54e0
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-05-12 18:15:59 +0530

    Add Engine Fires

---
+++ Assets/Scripts/Player Scripts/EngineFires.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Player_Scripts
+{
+    public class EngineFires : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
