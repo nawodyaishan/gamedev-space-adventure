b90d14a0806b024c41d5d026df8996c562e27f53
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-05-12 13:00:42 +0530

    Add FixedUpdate, HandleMovement and Awake Methods

--- Assets/Scripts/Player Scripts/PlayerMoveKeyboard.cs
+++ Assets/Scripts/Player Scripts/PlayerMoveKeyboard.cs
@@ -5,6 +5,20 @@ namespace Player_Scripts
     public class PlayerMoveKeyboard : MonoBehaviour
     {
         [SerializeField] private float speed = 600f;
-        private Rigidbody2D myBody;
+        private Rigidbody2D _myBody;
+
+        private void Awake()
+        {
+            _myBody = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            HandleMovement();
+        }
+
+        void HandleMovement()
+        {
+        }
     }
-} //Class
+} //Class 
