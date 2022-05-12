0b71642410a13ffe5f27582aa8ae02dc813cb97c
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-05-12 12:42:34 +0530

    Add PlayerMoveKeyboard.cs

--- Assets/Scripts/Player Scripts/PlayerMoveKeyboard.cs
+++ Assets/Scripts/Player Scripts/PlayerMoveKeyboard.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMoveKeyboard : MonoBehaviour
+namespace Player_Scripts
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerMoveKeyboard : MonoBehaviour
     {
-        
+        [SerializeField] private float speed = 600f;
+        private Rigidbody2D myBody;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+} //Class
