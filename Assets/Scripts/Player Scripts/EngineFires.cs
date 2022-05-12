75144ac3aaeb12b8fa432f81fa7b944f4dd7bff6
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-05-12 20:00:57 +0530

    Add Engine Fire

--- Assets/Scripts/Player Scripts/EngineFires.cs
+++ Assets/Scripts/Player Scripts/EngineFires.cs
@@ -6,5 +6,43 @@ namespace Player_Scripts
     {
         // To Attach All the Particles in Player Ship
         [SerializeField] private ParticleSystem[] engineFires;
+
+        // Engine Power
+        [SerializeField] private int enginePower = 3;
+
+        private void Update()
+        {
+            HandleFireEngine();
+        }
+
+        private void HandleFireEngine()
+        {
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                EmiEngineFireParticle(2, enginePower);
+                EmiEngineFireParticle(3, enginePower);
+            }
+
+            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            {
+                EmiEngineFireParticle(4, enginePower);
+                EmiEngineFireParticle(5, enginePower);
+            }
+
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                EmiEngineFireParticle(0, enginePower);
+            }
+
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                EmiEngineFireParticle(1, enginePower);
+            }
+        }
+
+        void EmiEngineFireParticle(int engineIndex, int enginePower)
+        {
+            engineFires[engineIndex].Emit(enginePower);
+        }
     }
 }
