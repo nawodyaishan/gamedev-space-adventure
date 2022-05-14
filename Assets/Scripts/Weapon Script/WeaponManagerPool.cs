fe4b32016def3ff3179f759050f51950699d26a4
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-05-14 17:15:04 +0530

    Update WeaponManagerPool.cs

--- Assets/Scripts/Weapon Script/WeaponManagerPool.cs
+++ Assets/Scripts/Weapon Script/WeaponManagerPool.cs
@@ -9,13 +9,19 @@ namespace Weapon_Script
         [SerializeField] private GameObject projectile;
         
         [SerializeField] List<GameObject> projectilePool = new List<GameObject>();
-        
+         
         
         
         
         private void Start()
         {
-             
+            for (int i = 0; i < 10; i++)
+            {
+                GameObject g = Instantiate(projectile);
+                projectilePool.Add(g);
+                
+            }
+
         }
         
         
