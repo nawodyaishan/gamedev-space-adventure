0a64d5bb25cb138a9837fbbef3b31d1ffcc77131
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-05-15 19:31:06 +0530

    Update EnemyMovement.cs

--- Assets/Scripts/Enemy Scripts/EnemyMovement.cs
+++ Assets/Scripts/Enemy Scripts/EnemyMovement.cs
@@ -1,14 +1,33 @@
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour
+namespace Enemy_Scripts
 {
-    // Start is called before the first frame update
-    void Start()
+    public class EnemyMovement : MonoBehaviour
     {
-    }
+        [SerializeField] private bool moveOnX, moveOnY;
+        [SerializeField] private float minX, maxX;
+        [SerializeField] private float minY, maxY;
 
-    // Update is called once per frame
-    void Update()
-    {
+        [SerializeField] private float moveSpeed = 8f;
+
+        [SerializeField] private float horizontalMovementTreshold = 8f;
+        [SerializeField] private float verticalMovementTreshold = 6f;
+
+        private Vector3 tempMovement_Horizontal;
+        private Vector3 tempMovement_Vertical;
+
+        private bool moveLeft;
+        [SerializeField] private bool moveUp = false;
+
+        private void Start()
+        {
+            var transform1 = transform;
+            var position = transform1.position;
+            minX = position.x - horizontalMovementTreshold;
+            maxX = position.x + horizontalMovementTreshold;
+
+            maxY = position.y = verticalMovementTreshold;
+            minY = position.y - verticalMovementTreshold;
+        }
     }
-}
+} //Class
