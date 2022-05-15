3a5ca4ca8f1882dba1419be0c427972274c0631e
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-05-15 09:39:33 +0530

    Add Player Shooting Projectile Spawn using Projectile Pool

---
+++ Assets/Scripts/Manager Scripts/TagManager.cs
@@ -0,0 +1,8 @@
+namespace Manager_Scripts
+{
+    public static class TagManager
+    {
+        public static string PLAYER_PROJECTILE_HOLDER_TAG = "PlayerProjectileHolder";
+        public static string ENEMY_PROJECTILE_HOLDER_TAG = "EnemyProjectileHolder";
+    }
+} // Class
--- Assets/Scripts/Weapon Script/WeaponManagerPool.cs
+++ Assets/Scripts/Weapon Script/WeaponManagerPool.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Manager_Scripts;
 using UnityEngine;
 
 namespace Weapon_Script
@@ -12,9 +13,82 @@ namespace Weapon_Script
         [SerializeField] private Transform projectileSpawnPoint;
         [SerializeField] private float shootWaitTime = 0.2f;
         [SerializeField] private bool isEnemy;
+        [SerializeField] private bool canShoot;
 
         private bool projectileSpawned;
-        private float shooterTimer;
-        private bool canShoot;
+        private float shootTimer;
+
+        private void Awake()
+        {
+            if (isEnemy)
+            {
+                projectileHolder = GameObject.FindWithTag(TagManager.ENEMY_PROJECTILE_HOLDER_TAG);
+            }
+            else
+            {
+                projectileHolder = GameObject.FindWithTag(TagManager.PLAYER_PROJECTILE_HOLDER_TAG);
+            }
+        }
+
+        private void Update()
+        {
+            if (Time.time > shootTimer)
+                canShoot = true;
+            HandlePlayerShooting();
+            HandleEnemyShooting();
+        }
+
+        private void HandlePlayerShooting()
+        {
+            if (!canShoot || isEnemy)
+                return;
+
+            if (Input.GetKeyDown(keyToPressToShoot))
+            {
+                GetObjectFromPoolOrGetANewOne();
+            }
+        }
+
+        private void GetObjectFromPoolOrGetANewOne()
+        {
+            for (int i = 0; i < projectilePool.Count; i++)
+            {
+                if (!projectilePool[i].activeInHierarchy)
+                {
+                    projectilePool[i].transform.position = projectileSpawnPoint.position;
+                    projectilePool[i].SetActive(true);
+                    projectileSpawned = true;
+                    break;
+                }
+                else
+                {
+                    projectileSpawned = false;
+                }
+            }
+
+            if (!projectileSpawned)
+            {
+                GameObject newProjectile = Instantiate(projectile, projectileSpawnPoint.position, Quaternion.identity);
+                projectilePool.Add(newProjectile);
+
+                newProjectile.transform.SetParent(projectileHolder.transform);
+
+                projectileSpawned = true;
+            }
+        }
+
+        void ResetShhotingTimer()
+        {
+            canShoot = false;
+
+            if (isEnemy)
+            {
+                shootTimer = Time.time + shootWaitTime;
+            }
+        }
+
+        private void HandleEnemyShooting()
+        {
+        }
     }
 } // Class
