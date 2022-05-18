a76ab46f29265cb46d23ff45a579648dba98da0a
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-05-18 15:54:41 +0530

    Add SelectRandomPosition, SetTargetPosition, SelectPointToPointPosition

--- Assets/Scripts/Enemy Scripts/EnemyMovementInPoints.cs
+++ Assets/Scripts/Enemy Scripts/EnemyMovementInPoints.cs
@@ -1,36 +1,61 @@
 using UnityEngine;
 
-public class EnemyMovementInPoints : MonoBehaviour
+namespace Enemy_Scripts
 {
-    [SerializeField] private Transform[] movementPoints;
-
-    private int currentMoveIndex;
-
-    private Vector3 targetPosition;
-
-    [SerializeField] private float moveSpeed = 8f;
-
-    private bool moveRandomly;
-
-    void SelectRandomPosition()
+    public class EnemyMovementInPoints : MonoBehaviour
     {
-    }
+        [SerializeField] private Transform[] movementPoints;
 
-    void SetTargetPosition()
-    {
-    }
+        private int currentMoveIndex;
 
-    void SelectPointToPointPosition()
-    {
-    }
+        private Vector3 targetPosition;
 
-    private void Move()
-    {
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        [SerializeField] private float moveSpeed = 8f;
 
-        if (Vector3.Distance(transform.position, targetPosition) < 0.1f)
+        private bool moveRandomly;
+
+        void SelectRandomPosition()
         {
-            SetTargetPosition
+            while (movementPoints[currentMoveIndex].position == targetPosition)
+            {
+                currentMoveIndex = Random.Range(0, movementPoints.Length);
+            }
+
+            targetPosition = movementPoints[currentMoveIndex].position;
+        }
+
+        void SetTargetPosition()
+        {
+            if (currentMoveIndex == movementPoints.Length)
+            {
+                currentMoveIndex = 0;
+            }
+
+            targetPosition = movementPoints[currentMoveIndex].position;
+
+            currentMoveIndex++;
+        }
+
+        void SelectPointToPointPosition()
+        {
+            if (moveRandomly)
+            {
+                SelectRandomPosition();
+            }
+            else
+            {
+                SelectPointToPointPosition();
+            }
+        }
+
+        private void Move()
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, targetPosition) < 0.1f)
+            {
+                SetTargetPosition();
+            }
         }
     }
 } // Class
