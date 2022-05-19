46203043f729c95c0fde6677a141602a2acda80a
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-05-19 12:54:58 +0530

    Update Tags

--- Assets/Scripts/Manager Scripts/TagManager.cs
+++ Assets/Scripts/Manager Scripts/TagManager.cs
@@ -5,5 +5,12 @@ namespace Manager_Scripts
         public static string PLAYER_PROJECTILE_HOLDER_TAG = "PlayerProjectileHolder";
         public static string ENEMY_PROJECTILE_HOLDER_TAG = "EnemyProjectileHolder";
         public static string PRIJECTILE_TAG = "Projectile";
+
+        public static string ENEMY_TAG = "Enemy";
+        public static string PLYAER_TAG = "Player";
+        public static string METEOR_TAG = "Meteor";
+        public static string UNTAGGED_TAG = "Untagged";
+        public static string PLAYER_HEALTH_SLIDER_TAG = "PlayerHealth";
+        public static string COLLECTABLE_TAG = "Collectable";
     }
 } // Class
