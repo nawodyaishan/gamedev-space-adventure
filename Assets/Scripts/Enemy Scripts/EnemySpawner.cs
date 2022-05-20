36d6b391f0331c6ab587d1d6adf7841cd75998fd
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-05-21 01:05:42 +0530

    Add SpawnNewWaveOfEnemies

--- Assets/Scripts/Enemy Scripts/EnemySpawner.cs
+++ Assets/Scripts/Enemy Scripts/EnemySpawner.cs
@@ -1,8 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Enemy_Scripts
 {
     public class EnemySpawner : MonoBehaviour
     {
+        public static EnemySpawner Instance;
+
+        [SerializeField] private GameObject[] enemies;
+
+        private List<GameObject> spawnedEnemies = new List<GameObject>();
+
+        [SerializeField] private Transform[] spawnPoints;
+
+        [SerializeField] private float spawnWaitTime = 2f;
+
+        private void Awake()
+        {
+            if (Instance == null)
+                Instance = this;
+        }
+
+        private void SpawnNewWaveOfEnemies()
+        {
+        }
+
+        IEnumerator _SpawnWave(float waitTime)
+        {
+            yield return new WaitForSeconds(waitTime);
+
+            SpawnNewWaveOfEnemies();
+        }
+
+
+        private void Start()
+        {
+        }
+
+        private void Update()
+        {
+        }
     }
 } // Class
