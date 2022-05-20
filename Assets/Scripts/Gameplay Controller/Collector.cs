b7fa32e40470eed628a0ccfef93e34db8e163026
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-05-20 19:26:15 +0530

    Update

--- Assets/Scripts/Gameplay Controller/Collector.cs
+++ Assets/Scripts/Gameplay Controller/Collector.cs
@@ -10,8 +10,11 @@ namespace Gameplay_Controller
             if (collision.CompareTag(TagManager.PRIJECTILE_TAG))
                 collision.gameObject.SetActive(false);
 
-            if (collision.CompareTag(TagManager.METEOR_TAG))
-                collision.gameObject.SetActive((false));
+            if (collision.CompareTag(TagManager.METEOR_TAG) || collision.CompareTag(TagManager.COLLECTABLE_TAG))
+            {
+                Destroy(collision.gameObject);
+            }
+            //collision.gameObject.SetActive((false));
         }
     }
 } //class
