2f489fd06eb7fc3f63a4ceb63a049651cab60e20
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-05-20 15:21:03 +0530

    Update

--- Assets/Scripts/Meteor Scripts/MeteorMovement.cs
+++ Assets/Scripts/Meteor Scripts/MeteorMovement.cs
@@ -1,14 +1,54 @@
 using UnityEngine;
 
-public class MeteorMovement : MonoBehaviour
+namespace Meteor_Scripts
 {
-    // Start is called before the first frame update
-    void Start()
+    public class MeteorMovement : MonoBehaviour
     {
-    }
+        [SerializeField] private float minSpeed = 4f, maxSpeed = 10f;
 
-    // Update is called once per frame
-    void Update()
-    {
+        private float speedX, speedY;
+
+        [SerializeField] bool moveOnX, moveOnY = true;
+
+        [SerializeField] private float minRotationSpeed = 15f, maxRotationSpeed = 50f;
+
+        [SerializeField] float rotationSpeed;
+
+        private Vector3 tempMovement;
+
+        private float zRotation;
+
+        private void Awake()
+        {
+            rotationSpeed = Random.Range(minRotationSpeed, maxRotationSpeed);
+
+            speedX = Random.Range(minSpeed, maxSpeed);
+            speedY = speedX;
+
+            if (Random.Range(0, 2) > 0)
+            {
+                // Changing Direction of Meteor
+                speedX *= -1;
+            }
+
+            if (Random.Range(0, 2) > 0)
+            {
+                rotationSpeed *= -1f;
+            }
+
+            if (Random.Range(0, 2) > 0)
+            {
+                moveOnX = true;
+            }
+        }
+
+
+        private void Start()
+        {
+        }
+
+        private void Update()
+        {
+        }
     }
-}
+} // Class
